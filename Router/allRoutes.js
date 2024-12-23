2e644f6c8d575ab7cf00d4911611ef05a0ebdb6d
ilkbkr67 <ilkay.bakar@gmail.com>
2024-12-23 22:31:09 +0100

    Commencement de la page "La Carte"

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/lacarte", "La Carte", "/pages/lacarte.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EMSAA - Snack/Epicerie";
